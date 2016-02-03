d063a8cc5bd4dd48c1aa93a934853f9303919749
Stuti Agrawal <stuti.agrawal@gmail.com>
2016-02-03 23:09:17 +0000

    style(run_cwl): adding a function call for download

--- slurm/run_cwl.py
+++ slurm/run_cwl.py
@@ -7,20 +7,31 @@ import setupLog
 import logging
 import tempfile
 
+def get_input_file(fromlocation, tolocation, logger):
+    """ download a file and return its location"""
+
+    exit_code = pipelineUtil.download_from_cleversafe(logger, fromlocation, tolocation)
+
+    if exit_code:
+        raise Exception("Cannot download file: %s" %(fromlocation))
+
+    outlocation = os.path.join(tolocation, os.path.basename(fromlocation))
+    return outlocation
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Run variant calling CWL")
     required = parser.add_argument_group("Required input parameters")
-    required.add_argument("--ref", default="/mnt/SRCATCH/index/GRCh38.d1.vd1.fa", help="path to reference genome")
-    required.add_argument("--normal", default=None, help="path to normal bam file")
-    required.add_argument("--tumor", default=None, help="path to tumor bam file")
-    required.add_argument("--normal_id", default=None, help="UUID for normal BAM")
-    required.add_argument("--tumor_id", default=None, help="UUID for tumor BAM")
-    required.add_argument("--case_id", default=None, help="UUID for case")
-    required.add_argument("--username", default=None, help="Username for postgres")
-    required.add_argument("--password", default=None, help="Password for postgres")
-    required.add_argument("--refindex", default="/mnt/SCRATCH/index/GRCh38.d1.vd1.fa.fai", help="Path to ref index on object store")
-    required.add_argument("--cwl", default=None, help="Path to CWL code")
+    required.add_argument("--ref", default=None, help="path to reference genome", required=True)
+    required.add_argument("--normal", default=None, help="path to normal bam file", required=True)
+    required.add_argument("--tumor", default=None, help="path to tumor bam file", required=True)
+    required.add_argument("--normal_id", default=None, help="UUID for normal BAM", required=True)
+    required.add_argument("--tumor_id", default=None, help="UUID for tumor BAM", required=True)
+    required.add_argument("--case_id", default=None, help="UUID for case", required=True)
+    required.add_argument("--username", default=None, help="Username for postgres", required=True)
+    required.add_argument("--password", default=None, help="Password for postgres", required=True)
+    required.add_argument("--refindex", default=None, help="Path to ref index on object store", required=True)
+    required.add_argument("--cwl", default=None, help="Path to CWL code", required=True)
 
     optional = parser.add_argument_group("Optional input parameters")
     optional.add_argument("--s3dir", default="s3://bioinformatics_scratch/", help="path to output files")
@@ -44,26 +55,29 @@ if __name__ == "__main__":
     log_file = os.path.join(workdir, "%s.somaticsniper.cwl.log" %str(vcf_uuid))
     logger = setupLog.setup_logging(logging.INFO, str(vcf_uuid), log_file)
 
-    #download bam files
-    reference = os.path.join(index, "GRCh38.d1.vd1.fa")
-    if not os.path.isfile(reference):
-        logger.info("getting reference")
-        pipelineUtil.download_from_cleversafe(logger, args.ref, index)
-        reference = os.path.join(index, os.path.basename(args.ref))
+    #logging inputs
+    logger.info("normal_bam_path: %s" %(args.normal))
+    logger.info("tumor_bam_path: %s" %(args.tumor))
+    logger.info("normal_bam_id: %s" %(args.normal_id))
+    logger.info("tumor_bam_id: %s" %(args.tumor_id))
+    logger.info("case_id: %s" %(args.case_id))
+    logger.info("vcf_id: %s" %(str(vcf_uuid)))
 
-    reference_index = os.path.join(index, "GRCh38.d1.vd1.fa.fai")
-    if not  os.path.isfile(reference_index):
-        logger.info("getting reference index")
-        pipelineUtil.download_from_cleversafe(logger, args.refindex, index)
-        reference_index = os.path.join(index, os.path.basename(args.refindex))
+    #download reference file
+    logger.info("getting reference: %s" %args.ref)
+    reference = get_input_file(args.ref, index, logger)
 
-    logger.info("getting normal bam")
-    pipelineUtil.download_from_cleversafe(logger, args.normal, inp)
-    bam_norm = os.path.join(inp, os.path.basename(args.normal))
+    #download reference index
+    logger.info("getting reference index: %s" %args.refindex)
+    reference_index = get_input_file(args.refindex, index, logger)
 
-    logger.info("geeting tumor bam")
-    pipelineUtil.download_from_cleversafe(logger, args.tumor,  inp)
-    bam_tumor = os.path.join(inp, os.path.basename(args.tumor))
+    #download normal bam
+    logger.info("getting normal bam: %s" %args.normal)
+    bam_norm = get_input_file(args.normal, inp, logger)
+
+    #download tumor bam
+    logger.info("getting tumor bam: %s" %args.tumor)
+    bam_tumor = get_input_file(args.tumor, inp, logger)
 
     os.chdir(workdir)
     #run cwl command
