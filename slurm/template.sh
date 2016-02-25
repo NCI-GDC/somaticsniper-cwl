8f74614ff11718ab9535ad6ba50d7c5dda570683
Stuti Agrawal <stuti.agrawal@gmail.com>
2016-02-25 20:08:03 +0000

    fix(template): fixing paths in template file

--- slurm/template.sh
+++ slurm/template.sh
@@ -14,12 +14,11 @@ refindex="s3://bioinformatics_scratch/GRCh38.d1.vd1.fa.fai"
 username="XX_username_XX"
 password="XX_password_XX"
 repository="git@github.com:NCI-GDC/somaticsniper-cwl.git"
-cwl="/home/ubuntu/somaticsniper-cwl/tools/somaticsniper-tool.cwl.yaml"
 s3dir="s3://washu_sniper_variant/"
-
-wkdir=`mktemp -d -p /home/ubuntu/SCRATCH` 
+wkdir=`mktemp -d -p /mnt/SCRATCH` 
 cd $wkdir 
-sudo git clone -b feat/slurm $repository somaticsniper-cwl  
+sudo git clone -b feat/slurm $repository  
 sudo chown ubuntu:ubuntu somaticsniper-cwl 
+cwl=$wkdir/somaticsniper-cwl/tools/somaticsniper-tool.cwl.yaml
 /home/ubuntu/.virtualenvs/p2/bin/python $wkdir/somaticsniper-cwl/slurm/run_cwl.py --ref $ref --refindex $refindex --normal $normal --tumor $tumor --normal_id $normal_id --tumor_id $tumor_id --case_id $case_id --username $username --password $password --basedir $basedir --cwl $cwl --s3dir $s3dir
 sudo rm -rf $wkdir
